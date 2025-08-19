6d0cae71779f86c4b51c1c46073cc2c7f238be43
Kawaii Richard <kawaiirichard@yandex.ru>
2025-08-19 21:56:27 +0300

    Добавил ссылку на мой гитхаб

--- note-app/src/components/Footer/Footer.jsx
+++ note-app/src/components/Footer/Footer.jsx
@@ -4,7 +4,16 @@ function Footer() {
   return (
     <>
       <div className={styles.footer}>
-        <div className={styles.copyright}>NoteApp 2025</div>
+        <div className={styles.copyright}>
+          <a
+            className={styles.link}
+            href="https://github.com/kawaiiRichard"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            my github
+          </a>
+        </div>
       </div>
     </>
   );
